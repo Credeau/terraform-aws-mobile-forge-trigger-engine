17676398bc4041d8a815aee00cb3261d1594b2f3
Harshit Sharma <brown.hash@outlook.com>
2025-07-22 16:38:27 +0530

    fix: remove duplicate placement group

--- placement_group.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_placement_group" "main" {
-  name            = format("%s-%s", var.application, var.environment)
-  strategy        = "partition"
-  partition_count = 3
-  tags            = local.common_tags
-}
